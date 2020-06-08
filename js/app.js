2fb508416c814ba2116330383bb87a3d059d9ed4
Dulce Villarreal <dulcedu@gmail.com>
2020-06-08 16:27:28 -0500

    add script app.js to index.html. Change app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 var app = new Vue({
+    //el=element
     el: '#app',
-    data: {},
+    data: {
+        hits:0,
+        questions:[
+            {
+                "id": 1,
+                "question": "What does CSS stand for?",
+                "nextId": 2,
+                "previousId": null,
+                "answer": "c",
+                "options": {
+                    "a": "Computer Style Sheets",
+                    "b": "Creative Style Sheets",
+                    "c": "Cascading Style Sheets",
+                    "d": "Colourful Style Sheets"
+                }
+            }
+        ]
+    },
     methods: {}
 })
