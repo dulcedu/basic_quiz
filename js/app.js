35ebc23302318ea9e2f775905908fe004dd29061
Dulce Villarreal <dulcedu@gmail.com>
2020-06-08 16:47:05 -0500

    add fake questions,I will change later

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var app = new Vue({
         questions:[
             {
                 "id": 1,
-                "question": "Who is Satoshi Nakamoto?",
+                "question": "What does CSS stand for?",
                 "nextId": 2,
                 "previousId": null,
                 "answer": "c",
@@ -16,7 +16,47 @@ var app = new Vue({
                     "c": "Cascading Style Sheets",
                     "d": "Colourful Style Sheets"
                 }
+            },
+            {
+                "id": 2,
+                "question": "Where in an HTML document is the correct place to refer to an external style sheet?",
+                "nextId": 2,
+                "previousId": 1,
+                "answer": "a",
+                "options": {
+                    "a": "In the <head> section",
+                    "b": "In the <body> section",
+                    "c": "At the end of the document",
+                    "d": "You can't refer to an external style sheet"
+                }
+            },
+            {
+                "id": 3,
+                "question": "Which HTML tag is used to define an internal style sheet?",
+                "nextId": 4,
+                "previousId": 2,
+                "answer": "a",
+                "options": {
+                    "a": "<style>",
+                    "b": "<script>",
+                    "c": "<headStyle>",
+                    "d": "<css>"
+                }
+            },
+            {
+                "id": 4,
+                "question": "Which HTML attribute is used to define inline styles?",
+                "nextId": null,
+                "previousId": 3,
+                "answer": "b",
+                "options": {
+                    "a": "class",
+                    "b": "style",
+                    "c": "font",
+                    "d": "styles"
+                }
             }
+            
         ]
     },
     methods: {}
